6deccbebe6f573e6c459a090861a51ac3b54ab3a
Manikandan <69749847+mani-cmd@users.noreply.github.com>
2025-03-27 15:00:45 +0530

    Update perfectNumber.py

--- Week-2/perfectNumber.py
+++ Week-2/perfectNumber.py
@@ -12,3 +12,25 @@ for i in range(n1, n2+1):
     if sum == i:
         print(i, end=" ")
     sum = 0
+
+""" using while loop"""
+# A perfect number is a number that equals the sum of its proper divisors (excluding itself)
+# He wants to identify all perfect numbers within a range
+
+n1 = int(input())
+n2 = int(input())
+# list1 = []
+sum1 = 0
+# for i in range(n1, n2+1):
+i = n1
+while i<=n2: 
+    # for x in range(1, i):
+    j = 1
+    while j<i :
+        if i%j == 0:
+            sum1+=j
+        j+=1
+    if sum1 == i:
+        print(i, end=" ")
+    i+=1
+    sum1 = 0
